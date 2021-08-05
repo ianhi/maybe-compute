a01ca64ad41c4b2d5433c825f7823cf2a8eb4bab
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2021-08-05 15:35:38 -0400

    trigger pre-commit

--- maybe_compute/__init__.py
+++ maybe_compute/__init__.py
@@ -2,7 +2,7 @@ try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
-__author__ = "Ian Hunt-Isaak"
+__author__ = 'Ian Hunt-Isaak'
 __email__ = "ianhuntisaak@gmail.com"
 
 from functools import wraps
